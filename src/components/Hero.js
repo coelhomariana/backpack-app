ca8af53693d94593a2b0580c67b2054feda697e6
Mariana Coelho <coelhomariana@outlook.com>
2023-03-27 16:40:44 -0300

    adding the files]

---
+++ src/components/Hero.js
@@ -0,0 +1,17 @@
+const Hero = () => {
+  return (
+    <section className='hero' id='home'>
+      <div className='hero-banner'>
+        <h1>backpack app</h1>
+        <p>
+        We specialize in selling a wide variety of tours and travel experiences to customers all around the world. 
+        
+        </p>
+        <a href='#tours' className='btn hero-btn '>
+          explore tours
+        </a>
+      </div>
+    </section>
+  )
+}
+export default Hero
